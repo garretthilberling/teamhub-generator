0c13039d87d2c6ced30c41eb7cfc8123d25ae21a
garretthilberling <ghilberling@knights.ucf.edu>
2022-02-27 15:36:29 -0500

    finished generating manager object with inquirer prompts. Encountering email: undefined bug when console logging the object if you select "no" for typing in ID.

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -2,16 +2,8 @@ const Manager = require('../lib/Manager');
 
 test('creates a manager object', () => {
     const manager = new Manager('Bob', '123-456-7890');
+    const numberInput = '123-456-7890';
 
     expect(manager.name).toBe('Bob');
-    expect(manager.id.length).toEqual(7);
-    expect(manager.getRandom()).toEqual(expect.any(Number));
+    expect(manager.isValidPhoneNumber(numberInput)).toBeTruthy(); //ensures phone number is in correct format
 });
-
-test('ensure phone number is in correct format', () => {
-    const manager = new Manager('Bob', '1234567', 'bob@gmail.com', '123-456-7890');
-
-    expect(manager.isValidPhoneNumber()).toBeTruthy();
-});
-
-
--- __test__/Member.test.js
+++ __test__/Member.test.js
@@ -4,9 +4,11 @@ test('creates member object', () => {
     const member = new Member('Bob','bob@gmail.com');
     const emailInput = 'bob@gmail.com';
     const idInput = 1234567;
+    const idInputStr = '1234567';
 
     expect(member.name).toBe('Bob');
     expect(member.isValidID(idInput)).toBeTruthy(); //making sure ID is a 7 digit integer
+    expect(member.isValidID(parseInt(idInputStr))).toBeTruthy(); //showing it also accepts strings
     expect(member.isValidEmail(emailInput)).toBeTruthy(); //making sure email is in valid format
 });
 
