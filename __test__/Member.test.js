5f35706d237040f2b805420e453a7e997a8ef045
garretthilberling <ghilberling@knights.ucf.edu>
2022-02-27 01:56:57 -0500

    done for the night

--- __test__/Member.test.js
+++ __test__/Member.test.js
@@ -2,9 +2,11 @@ const Member = require('../lib/Member');
 
 test('creates member object', () => {
     const member = new Member('Bob','bob@gmail.com');
+    const emailInput = 'bob@gmail.com';
+    const idInput = '1234567';
 
     expect(member.name).toBe('Bob');
-    expect(member.isValidID).toBeTruthy(); //making sure ID is a 7 digit integer
-    expect(member.isValidEmail()).toBeTruthy(); //making sure email is in valid format
+    expect(member.isValidID(idInput)).toBeTruthy(); //making sure ID is a 7 digit integer
+    expect(member.isValidEmail(emailInput)).toBeTruthy(); //making sure email is in valid format
 });
 
