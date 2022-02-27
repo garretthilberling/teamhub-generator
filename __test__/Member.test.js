c1e659e95bee23807158f9de270cfb2fb919a7ab
garretthilberling <ghilberling@knights.ucf.edu>
2022-02-27 13:41:14 -0500

    reached out to TA waiting for response. Still trying to get validate to work correctly with the object methods

--- __test__/Member.test.js
+++ __test__/Member.test.js
@@ -3,7 +3,7 @@ const Member = require('../lib/Member');
 test('creates member object', () => {
     const member = new Member('Bob','bob@gmail.com');
     const emailInput = 'bob@gmail.com';
-    const idInput = '1234567';
+    const idInput = 1234567;
 
     expect(member.name).toBe('Bob');
     expect(member.isValidID(idInput)).toBeTruthy(); //making sure ID is a 7 digit integer
