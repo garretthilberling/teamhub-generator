91c1e901d0c2a2de37a3d9688e0610332943cfc6
garretthilberling <ghilberling@knights.ucf.edu>
2022-02-28 17:34:48 -0500

    Had to change format of questionnaire to always allow the option to alternate between entering engineers and interns. Just have to make the html and it is done.

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -2,8 +2,10 @@ const Intern = require('../lib/Intern');
 
 test('creates a intern object', () => {
     const intern = new Intern('Billie', '1234567','billie@knights.ucf.edu','UCF');
+    const school = 'UCF';
 
     expect(intern.name).toBe('Billie');
     expect(intern.id.length).toEqual(7);
     expect(intern.school).toEqual(expect.any(String));
+    expect(intern.isValidSchool(school)).toBeTruthy();
 });
